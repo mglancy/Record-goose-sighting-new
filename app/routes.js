7fe88ecca482d4fcfc670557f4371f50e1381915
mglancy <glancy@gmail.com>
2023-03-30 13:53:55 +0100

    Move old version files into new kit
    Mostly a copy-paste of the previous code but with some changes to use new kit features. Not fully tested.

--- app/routes.js
+++ app/routes.js
@@ -6,4 +6,38 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-// Add your routes here
+// Routes for record a goose steps
+
+router.get('steps/start', function(request, response) {
+  response.render('steps/start')
+})
+
+router.get('steps/geese-liked', function(request, response) {
+  response.render('steps/geese-liked')
+})
+
+router.get('steps/goose-type', function(request, response) {
+  response.render('steps/goose-type')
+})
+
+router.get('steps/date-saw-goose', function(request, response) {
+  response.render('steps/date-saw-goose')
+})
+
+router.get('steps/check-your-answers', function(request, response) {
+  response.render('steps/check-your-answers')
+})
+
+router.get('steps/end', function(request, response) {
+  response.render('steps/end')
+})
+
+router.get('steps/worksheet', function(request, response) {
+  response.render('steps/worksheet')
+})
+
+router.get('steps/answers', function(request, response) {
+  response.render('steps/answers')
+})
+
+module.exports = router
